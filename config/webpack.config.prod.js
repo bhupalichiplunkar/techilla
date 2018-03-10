8a7ce625dfb896041c4869204b13e1bb0ea42c75
Bhupali <bhupali.chiplunkar@gmail.com>
2018-03-10 21:25:53 +0530

    - change public path

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -66,7 +66,7 @@ module.exports = {
     filename: '[name].[chunkhash:8].js',
     chunkFilename: '[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
-    publicPath: publicPath,
+    publicPath: '/',
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info =>
       path
